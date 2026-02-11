1e3434e8d5281799e1f7e95cf99be0af9c9289e5
Adam Duvick <adamduvick@gmail.com>
2026-02-10 22:50:03 -0600

    feat: test label story

--- stories/leptos/src/primitives/label.rs
+++ stories/leptos/src/primitives/label.rs
@@ -1,28 +1,25 @@
 use leptos::prelude::*;
 use radix_leptos_label::*;
-use tailwind_fuse::*;
+
+stylance::import_crate_style!(label_classes, "src/primitives/label.stories.module.css");
 
 #[component]
 pub fn Styled() -> impl IntoView {
-    let root_class = Memo::new(move |_| RootClass::default().to_class());
-
     view! {
-        <Label attr:class=root_class>Label</Label>
+        <Label attr:class=label_classes::root>Label</Label>
     }
 }
 
 #[component]
 pub fn WithControl() -> impl IntoView {
-    let control_class = Memo::new(move |_| ControlClass::default().to_class());
-
     view! {
         <h1>Wrapping control</h1>
         <Label>
-            <Control attr:class=control_class /> Label
+            <Control /> " Label"
         </Label>
 
         <h1>Referencing control</h1>
-        <Control attr:id="control" attr:class=control_class />
+        <Control attr:id="control" />
         <Label attr:r#for="control">Label</Label>
     }
 }
@@ -31,7 +28,7 @@ pub fn WithControl() -> impl IntoView {
 pub fn WithInputNumber() -> impl IntoView {
     view! {
         <Label>
-            <span>Name:</span>
+            <span>"Name:"</span>
             <input type="number" />
         </Label>
     }
@@ -40,20 +37,11 @@ pub fn WithInputNumber() -> impl IntoView {
 #[component]
 fn Control() -> impl IntoView {
     view! {
-        <button on:click=move |_| window().alert_with_message("clicked").expect("Alert should be successful.")>
-            Control
+        <button
+            class=label_classes::control
+            on:click=move |_| window().alert_with_message("clicked").expect("Alert should be successful.")
+        >
+            "Control"
         </button>
     }
 }
-
-#[derive(TwClass, Default, Clone, Copy)]
-#[tw(
-    class = "inline-block align-middle cursor-default border-[1px] border-solid border-[gainsboro] p-[10px]"
-)]
-struct RootClass {}
-
-#[derive(TwClass, Default, Clone, Copy)]
-#[tw(
-    class = "inline-flex border-[1px] border-solid border-[gainsboro] p-[10px] align-middle m-[0px_10px] hover:bg-[red]"
-)]
-struct ControlClass {}
